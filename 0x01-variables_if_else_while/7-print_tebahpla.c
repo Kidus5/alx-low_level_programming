cebfc6b0e1b87152c47cef575850d1393a9f52b4
Saint Forever <kiduszelalem5@gmail.com>
2022-09-10 07:38:44 -0700

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char pla = 'z';
+
+	while (pla >= 'a')
+	{
+		putchar(pla);
+		pla--;
+	}
+	putchar('\n');
+	return (0);
+}
