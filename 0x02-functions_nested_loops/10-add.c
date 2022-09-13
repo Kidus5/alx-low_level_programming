6b4cf8965830d0a7c90bbb6120b72fb90697b60a
Saint Forever <kiduszelalem5@gmail.com>
2022-09-13 12:03:22 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
  * add - adds two integers
  * @a: the first integer
  * @b: the second integer
- * 
+ *
  * Return: the result
  */
 int add(int a, int b)
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,15 +23,15 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (result < 10)
-			{
-				_putchar(' ');
-				_putchar(result + '0');
-			}
-			else
+			if (result > 9)
 			{
 				_putchar(result / 10 + '0');
 				_putchar(result % 10 + '0');
+			}
+			else if (result < 10 || mul != 0)
+			{
+				_putchar(' ');
+				_putchar(result + '0');
 			}
 			mul++;
 		}
