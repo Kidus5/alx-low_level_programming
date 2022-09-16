a89b1d6cdda607dcfe9700c6481967fb208c69e8
Saint Forever <kiduszelalem5@gmail.com>
2022-09-16 14:14:56 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ *
+ * Return: Nothing.
+ */
+void print_numbers(void)
+{
+	int num = 48;
+
+	for (num = 48; num < 58; num++)
+		_putchar(num);
+	_putchar('\n');
+}
