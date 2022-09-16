fc3d6d1acdfacaf0756b33c465477ee80f9fc289
Saint Forever <kiduszelalem5@gmail.com>
2022-09-16 15:11:19 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers form 1 - 100 if Multiple of 3 Fizz
+ * 	Multiple of 5 Buzz and Multiple of both Fizzbuzz
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0 && i != 100)
+			printf("Buzz ");
+		else if (i == 100)
+			printf("Buzz");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+}
