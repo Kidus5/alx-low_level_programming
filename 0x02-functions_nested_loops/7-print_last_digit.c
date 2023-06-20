d7fa105c4b92cac4950061daf1e15102376c7b4d
Kidus Zelalem <kiduszelalem5@gmail.com>
2023-06-20 16:53:51 +0300

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @i: the integer
+ *
+ * Return: the value of the last digit
+ */
+int print_last_digit(int i)
+{
+	if (i >= 0)
+	{
+		_putchar(i % 10 + '0');
+		return (i % 10);
+	}
+	else
+	{
+		i = -i;
+		_putchar(i % 10 + '0');
+		return (i % 10);
+	}
+}
