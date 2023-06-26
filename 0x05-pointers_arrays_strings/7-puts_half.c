a68d2426ad4538450e4e1f6e9004b772e90b2bc7
Kidus Zelalem <kiduszelalem5@gmail.com>
2023-06-26 11:09:13 +0300

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: the string
+ *
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int n, len;
+	len = _strlen(str);
+
+	if (len % 2 == 0)
+	{
+		for (n = len / 2; str[n]; n++)
+			_putchar(str[n]);
+	}
+	else
+	{
+		for (n < ((len -1) / 2); str[n]; n++)
+			_putchar(str[n]);
+	}
+	_putchar('\n');
+}
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len]; len++)
+		;
+	return (len);
+}
