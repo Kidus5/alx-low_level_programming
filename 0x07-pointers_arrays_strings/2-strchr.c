e4238455fb7b3ccf9d400805c20b7d6330951bfe
Kidus Zelalem <kiduszelalem5@gmail.com>
2023-07-04 16:28:19 +0300

    strspn

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: a pointer to a string
+ * @c: the character to be located
+ *
+ * Return: a pointer to the first occurrence of the character
+ */
+char *_strchr(char *s, char c)
+{
+	int i, len, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c && s[i] != '\0')
+		{
+		}
+	}
+	return (s);
+}
+
