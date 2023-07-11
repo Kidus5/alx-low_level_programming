41a95f0d2cc217b761f4ce4891e05479bc45b224
Kidus Zelalem <kiduszelalem5@gmail.com>
2023-07-11 22:35:25 +0300

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,7 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int i, j;
 
-	ptr = malloc(sizeof(char *) + 1);
+	ptr = malloc(sizeof(s1 + s2) + 1);
 	if (ptr == NULL)
 		return (NULL);
 	if (s1 == NULL || s2 == NULL)
