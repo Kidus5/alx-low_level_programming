5141aed7f966f520a557f5bbfd444e20cc74e7f2
Kidus Zelalem <kiduszelalem5@gmail.com>
2023-07-13 22:15:31 +0300

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,10 +12,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 	unsigned int i;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
-		return (NULL);
-	if (nmemb == 0 || size == 0)
 		return (NULL);
 	for (i = 0; i < size * nmemb; i++)
 	{
